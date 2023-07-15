28c65afdf9415c57ec06da7bc54eb3b16a5801c5
Spycrab-db <gavinyan2004@gmail.com>
2023-07-15 17:54:18 -0400

    Restructured code with util function

--- src/about.js
+++ src/about.js
@@ -2,17 +2,14 @@ import team from './assets/team.jpg';
 import './css/about.css';
 import { createElement } from './util';
 
-const title = document.createElement('h1');
-title.textContent = "Our Story"
+const title = createElement('h1', "Our Story");
 
 const img = document.createElement('img');
 img.src = team;
 
-const subHeading = document.createElement('h2');
-subHeading.textContent = "Our Business Philosophy"
+const subHeading = createElement('h2', "Our Business Philosophy");
 
-const description = document.createElement('p');
-description.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+const description = createElement('p', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
 export default () =>{
     const root = document.querySelector('#content');
--- src/home.js
+++ src/home.js
@@ -2,8 +2,9 @@ import bicycle1 from './assets/bicycle1.jpg';
 import './css/home.css'
 import { createElement } from './util';
 
-const title = document.createElement('h1');
-title.textContent = "The World's Greatest Product";
+const root = document.querySelector('#content');
+
+const title = createElement('h1', "The World's Greatest Product");
 
 function imageContainer() {
     const element = document.createElement('div');
@@ -11,11 +12,10 @@ function imageContainer() {
     const image = document.createElement('img');
     image.src = bicycle1;
 
-    const backButton = document.createElement('div')
-    backButton.innerText = "<";
+    const backButton = createElement('div', "<")
 
-    const frontButton = document.createElement('div')
-    frontButton.innerText = ">";
+    const frontButton = createElement('div', ">")
+    
     element.appendChild(backButton);
     element.appendChild(image);
     element.appendChild(frontButton);
@@ -23,16 +23,13 @@ function imageContainer() {
     return element;
 }
 
-const subHeading = document.createElement('h2');
-subHeading.textContent = "Created with the Most Innovative Technology"
+const subHeading = createElement('h2', "Created with the Most Innovative Technology");
 
-const description = document.createElement('p');
-description.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+const description = createElement('p', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
-const root = document.querySelector('#content');
+
 
 export default () => {
-    const root = document.querySelector("#content");
     root.appendChild(title);
     root.appendChild(imageContainer());
     root.appendChild(subHeading);
